47632e96d1fd2469fb90eb9fe7f3913df5d499ce
Zack <Zackcolemanmusic@gmail.com>
2017-10-11 20:15:45 +0100

    added save

--- models/artists.rb
+++ models/artists.rb
@@ -1,17 +1,22 @@
 require('pg')
-
+require_relative('../sql_runner.rb')
 
 class Artists
 
   attr_reader(:name)
 
   def initialize(options)
-
+    @id = options['id'].to_i if options['id']
     @name = options('name')
 
   end
 
+  def save()
 
+      sql = "INSERT INTO customers(name) VALUES ($1) RETURNING *"
 
+      values = [@name]
 
+      @id = SqlRunner.run(sql, values)[0]['id'].to_i()
+  end
 end
